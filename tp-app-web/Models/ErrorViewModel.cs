468b01172a827f04a8d4de9ae51bdbc295e7b7bd
Tomas-Montero-davinci <tomas.montero@davinci.edu.ar>
2022-07-07 18:12:51 -0300

    Inicio app web

---
+++ tp-app-web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tp_app_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
